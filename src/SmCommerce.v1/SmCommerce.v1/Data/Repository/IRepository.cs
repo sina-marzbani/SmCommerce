6f6f509320885a6705491ae14535d1e394b2f141
sina marzbani <sina.marzbani67@gmail.com>
2020-02-17 15:34:36 +0330

    complete repository class

--- src/SmCommerce.v1/SmCommerce.v1/Data/Repository/IRepository.cs
+++ src/SmCommerce.v1/SmCommerce.v1/Data/Repository/IRepository.cs
@@ -8,8 +8,8 @@ namespace SmCommerce.v1.Data.Repository
 {
     public interface IRepository<T> where T : class
     {
-        T Get(int id);
-        IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null);
+        T GetById(int id);
+        IEnumerable<T> GetList(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null);
         T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null);
         void Add(T entity);
         void Remove(int id);
--- src/SmCommerce.v1/SmCommerce.v1/Data/Repository/Repository.cs
+++ src/SmCommerce.v1/SmCommerce.v1/Data/Repository/Repository.cs
@@ -1,11 +1,73 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace SmCommerce.v1.Data.Repository
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly DbContext _dbContext;
+        internal DbSet<T> _dbSet;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+        public IEnumerable<T> GetList(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+                return orderBy(query).ToList();
+
+            return query.ToList();
+        }
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return query.FirstOrDefault();
+        }
+        public void Remove(int id)
+        {
+            T entity = GetById(id);
+            Remove(entity);
+        }
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
     }
 }
