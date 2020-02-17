292946a8ba9253fdb6bb2557402fac0d67d791bf
sina marzbani <sina.marzbani67@gmail.com>
2020-02-17 08:45:49 +0330

    ProductCategory add display names

--- src/SmCommerce.v1/SmCommerce.v1/Models/ProductCategory.cs
+++ src/SmCommerce.v1/SmCommerce.v1/Models/ProductCategory.cs
@@ -12,24 +12,24 @@ namespace SmCommerce.v1.Models
     {
         [Required]
         [MaxLength(256)]
+        [Display(Name = "عنوان")]
         public string Title { get; set; }
 
-
         public int? ParentId { get; set; }
-
 
         [ForeignKey("ParentId")]
         public ProductCategory ProductCategoryParent { get; set; }
 
-
+        [Display(Name = "ترتیب نمایش در منو")]
         public int DisplayOrder { get; set; }
 
+        [Display(Name = "منتشر کردن")]
         public bool Published { get; set; }
 
         [Required]
         public DateTime ModifiedDate { get; set; }
 
-
+        [Display(Name = "توضیحات")]
         public string Description { get; set; }
     }
 }
