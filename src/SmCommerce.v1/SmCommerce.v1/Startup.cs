90d8c30b49b8ea3fa83e262a0bf7bb7388feae2d
sina marzbani <sina.marzbani67@gmail.com>
2020-02-17 18:00:49 +0330

    Startup.cs modified

--- src/SmCommerce.v1/SmCommerce.v1/Startup.cs
+++ src/SmCommerce.v1/SmCommerce.v1/Startup.cs
@@ -12,33 +12,88 @@ using SmCommerce.v1.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SmCommerce.v1.Data.Repository;
 
 namespace SmCommerce.v1
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
-            services.AddRazorPages();
-        }
+            #region [ Cookie ]
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //});
+            #endregion
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            #region [ ApplicationDbContext ]
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            #endregion
+
+            #region [ Identity ]
+            // Default:
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //        .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    // If use AddDefaultIdentity method, there is TokenProviders by default and it's not needed to add below method.
+                    // below method allow use two factor authentication or token system
+                    .AddDefaultTokenProviders();
+            // It's not needed to add from .netcore 3.0 onwards
+            //.AddDefaultUI(UIFramework.Bootstrap4);
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = $"/Identity/Account/Login";
+                options.LogoutPath = $"/Identity/Account/Logout";
+                options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+            });
+
+            //services.AddSingleton<IEmailSender, EmailSender>();
+            #endregion
+
+            #region [ UnitOfWork ]
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            #endregion
+
+            #region [ DbInitializer ]
+            //services.AddScoped<IDbInitializer, DbInitializer>();
+            #endregion
+
+            #region [ Session ]
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            #endregion
+
+            #region [ Razor ]
+            // Default:
+            //services.AddControllersWithViews()
+            //services.AddRazorPages();
+            services.AddControllersWithViews()
+                    .AddNewtonsoftJson()
+                    .AddRazorRuntimeCompilation(); // brfore asp.net core 3.0 has by default
+
+            services.AddRazorPages();
+            services.AddMvc();
+            #endregion            
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseRouting();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -53,8 +108,13 @@ namespace SmCommerce.v1
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseRouting();
+            app.UseSession();
+            //app.UseCookiePolicy();
 
+            //------------------------------
+            // **How to call this method just one time?**
+            //dbInitializer.Initialize();
+            //------------------------------
             app.UseAuthentication();
             app.UseAuthorization();
 
@@ -62,7 +122,7 @@ namespace SmCommerce.v1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Admin}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
